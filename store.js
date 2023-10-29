311435639f369adf8c25b5e13e1e459763ff33e9
MastooraTurkmen <132576850+MastooraTurkmen@users.noreply.github.com>
2023-10-29 07:40:38 +0430

    first commit

--- store.js
+++ store.js
@@ -32,9 +32,5 @@ function favoritesReducer(state = initialState, action) {
   }
 }
 
-// const action = { type: "ADD_FAVORITE", payload: { favorite: { title: "story1", id: 1 } } };
-
 const store = createStore(favoritesReducer);
-// store.dispatch(action);
-// console.log(store.getState());
 export default store;
